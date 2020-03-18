e09c23cbd2d446fbdd3467ce52e2b3aa4323bfc4
Kirill Sorokin <k.sorokin@hotmail.com>
2020-03-18 09:56:46 +0300

    Update check status change function

--- notifiers/common.go
+++ notifiers/common.go
@@ -14,7 +14,8 @@ func getMessage(updates []watcher.URLUpdate) string {
 		if idx != 0 {
 			message.WriteRune('\n')
 		}
-		message.WriteString(fmt.Sprintf("%v %v: ", update.Created.Round(time.Second), update.Old.Link))
+		message.WriteString(fmt.Sprintf("%v %v: ",
+			update.Created.Round(time.Second).UTC(), update.Old.Link))
 		if errText := update.Error(); errText != nil {
 			message.WriteString(*errText)
 		} else {
@@ -26,6 +27,13 @@ func getMessage(updates []watcher.URLUpdate) string {
 
 func checkStatusChange(update watcher.URLUpdate) bool {
 	if update.Old.Good() != update.New.Good() {
+		// status changed
+		return true
+	}
+	if !update.New.Good() &&
+		(update.New.Status != update.Old.Status ||
+			update.New.Err != update.Old.Err) {
+		// error changed
 		return true
 	}
 	return false
